ef8faa3152a8332aa3050e422b94dd005f02b6ac
Gianmario Iamoni <gianmario.iamoni@gmail.com>
2024-01-02 13:25:20 +0100

    implement showe

--- app.js
+++ app.js
@@ -10,16 +10,27 @@ app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
     res.render("home");
-})
+});
 
 app.get("/campgrounds", async (req, res) => {
     const campgrounds = await Campground.find({});
     res.render("campgrounds/index", { campgrounds });
-})
+});
+
+app.get("/campgrounds/new", (req, res) => {
+    res.render("campgrounds/new");
+});
+
+
+app.get("/campgrounds/:id", async (req, res) => {
+    const campground = await Campground.findById(req.params.id)
+    res.render("campgrounds/show", { campground });
+});
+
 
 app.listen(3000, () => {
     console.log("SERVING ON PORT 3000");
-})
+});
 
 main()
     .catch(err => console.log(err));
