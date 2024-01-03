4dd31d102d9748932ee3e6e7da001de042767708
Gianmario Iamoni <gianmario.iamoni@gmail.com>
2024-01-03 12:32:46 +0100

    implement edit

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const mongoose = require('mongoose');
+const methodOverrride = require("method-override");
 const Campground = require("./models/campground");
 
 
@@ -9,6 +10,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverrride("_method"));
 
 app.get("/", (req, res) => {
     res.render("home");
@@ -19,10 +21,12 @@ app.get("/campgrounds", async (req, res) => {
     res.render("campgrounds/index", { campgrounds });
 });
 
+// new - route serving the new form
 app.get("/campgrounds/new", (req, res) => {
     res.render("campgrounds/new");
 });
 
+// new - route fro post request
 app.post("/campgrounds/", async (req, res) => {
     const campground = new Campground(req.body.campground);
     await campground.save();
@@ -30,6 +34,20 @@ app.post("/campgrounds/", async (req, res) => {
 
 });
 
+// edit - route serving the edit form
+app.get("/campgrounds/:id/edit", async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/edit', { campground });
+});
+
+
+// edit - route fro post request
+app.put("/campgrounds/:id", async (req, res) => {
+    const campground = await Campground.findByIdAndUpdate(req.params.id, { ...req.body.campground });
+        
+    res.redirect(`/campgrounds/${campground._id}`);
+
+});
 
 app.get("/campgrounds/:id", async (req, res) => {
     const campground = await Campground.findById(req.params.id)
