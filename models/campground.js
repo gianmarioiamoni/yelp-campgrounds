18a169720884eacbf71cbafa106be89b9c1f5229
Gianmario Iamoni <gianmario.iamoni@gmail.com>
2024-01-19 17:25:08 +0100

    add thumbnails for delete images

--- models/campground.js
+++ models/campground.js
@@ -3,16 +3,20 @@ const Review = require("./review");
 const Schema = mongoose.Schema;
 
 
+const ImageSchema = new Schema({
+    url: String,
+    filename: String
+});
 
+// virtual for thumbnails
+// It shows the information as It would be stored in the DB
+ImageSchema.virtual("thumbnail").get(function () {
+    return this.url.replace("/upload", "/upload/w_200");
+});
 
 const CampgroundSchema = new Schema({
     title: String,
-    images: [
-        {
-            url: String,
-            filename: String
-        }
-    ],
+    images: [ImageSchema],
     price: Number,
     description: String,
     location: String,
