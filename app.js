d7ed6188796f884e3dffb982cfc852ba9127d3bf
Gianmario Iamoni <gianmario.iamoni@gmail.com>
2024-01-24 17:58:10 +0100

    add Helmet

--- app.js
+++ app.js
@@ -24,6 +24,8 @@ const LocalStrategy = require("passport-local");
 // solve mongo injection security issue
 const mongoSanitize = require('express-mongo-sanitize');
 
+const helmet = require("helmet");
+
 const User = require("./models/user");
 
 
@@ -44,6 +46,58 @@ const sessionConfig = {
 };
 
 app.use(session(sessionConfig));
+
+// HELMET
+// defines non-self sources to allow
+const scriptSrcUrls = [
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://api.mapbox.com/",
+    "https://kit.fontawesome.com/",
+    "https://kit-free.fontawesome.com/",
+    "https://cdnjs.cloudflare.com/",
+    "https://cdn.jsdelivr.net",
+    "https://cdn.jsdelivr.net/"
+];
+const styleSrcUrls = [
+    "https://kit-free.fontawesome.com/",
+    "https://kit-free.fontawesome.com/",
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.mapbox.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://fonts.googleapis.com/",
+    "https://use.fontawesome.com/",
+    "https://cdn.jsdelivr.net/"
+];
+const connectSrcUrls = [
+    "https://api.mapbox.com/",
+    "https://a.tiles.mapbox.com/",
+    "https://b.tiles.mapbox.com/",
+    "https://events.mapbox.com/",
+];
+const fontSrcUrls = [];
+// Use Helmet: enables all 11 middlewares helmet comes with
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: [],
+            connectSrc: ["'self'", ...connectSrcUrls],
+            scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+            styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+            workerSrc: ["'self'", "blob:"],
+            objectSrc: [],
+            imgSrc: [
+                "'self'",
+                "blob:",
+                "data:",
+                "https://res.cloudinary.com/dzmynvqbz/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+                "https://images.unsplash.com/",
+            ],
+            fontSrc: ["'self'", ...fontSrcUrls],
+        },
+    })
+);
+
 app.use(flash());
 
 // Passport initialization; session should be initialized before
@@ -75,6 +129,8 @@ app.use((req, res, next) => {
 // - req.headers
 // - req.query
 app.use(mongoSanitize());
+
+
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
