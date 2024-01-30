2b29d79b1d631e378049cb0785e70897e49a217e
Gianmario Iamoni <gianmario.iamoni@gmail.com>
2024-01-30 16:28:00 +0100

    basic search

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -7,7 +7,20 @@ const mapBoxToken = process.env.MAPBOX_TOKEN;
 const geocoder = mbxGeocoding({ accessToken: mapBoxToken });
 
 module.exports.index = async (req, res) => {
-    const campgrounds = await Campground.find({});
+    let dbQuery = {};
+    if (req.query.search) {
+        // we have a not null search query
+        console.log("req.query = ", req.query);
+        dbQuery = {
+            $or: [
+                { title: { $regex: req.query.search } },
+                { location: { $regex: req.query.search } },
+                { description: { $regex: req.query.search } }]
+        } 
+
+    }
+    // const campgrounds = await Campground.find({});
+    const campgrounds = await Campground.find(dbQuery);
     res.render("campgrounds/index", { campgrounds });
 }
 
@@ -15,34 +28,6 @@ module.exports.renderNewForm = (req, res) => {
     res.render("campgrounds/new");
 }
 
-// module.exports.createCampground = async (req, res, next) => {
-//     // forwarding geocoding provided by Mapbox
-//     const geoData = await geocoder.forwardGeocode({
-//         query: req.body.campground.location,
-//         limit: 1
-//     }).send();
-
-//     // create a new Campground object 
-//     const campground = new Campground(req.body.campground);
-    
-//     // body.features[0].geometry.coordinates is an array [longitude, latitude]
-//     // (opposite of the expected order!)
-//     campground.geometry = geoData.body.features[0].geometry.coordinates;
-//     console.log("req.body = " + JSON.stringify(req.body));
-//     console.log("req.body.features = " + JSON.stringify(req.body.features));
-//     console.log("campground.geometry = " + campground.geometry);
-//     // req.files it's an array containing information about loaded files provided by Multer
-//     campground.images = req.files.map(f => ({ url: f.path, filename: f.filename }));
-//     // add the author as the current logged user; the id is stored in req.user, provided by Passport
-//     campground.author = req.user._id;
-
-//     await campground.save();
-//     console.log(campground);
-
-//     req.flash("success", "Successfully made a new campground");
-
-//     res.redirect(`/campgrounds/${campground._id}`);
-// }
 module.exports.createCampground = async (req, res, next) => {
     const query = req.body.campground.location;
     const mapboxAPIEndpoint = `https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?access_token=${mapBoxToken}`;
@@ -140,6 +125,7 @@ module.exports.deleteCampground = async (req, res) => {
     req.flash("success", "Successfully deleted campground");
     res.redirect("/campgrounds");
 }
+
 
 
 
